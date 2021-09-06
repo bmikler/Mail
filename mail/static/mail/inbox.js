72ad05020bd686f3c323fc236fd2dcd470601cf7
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-09-06 20:51:43 +0200

    read mail function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,38 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  // By default, load the inbox
+  load_mailbox('inbox');
+
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-  document.querySelector('#compose-form').onsubmit = function () {
 
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value,
-      })
-    })
-      .then(response => response.json())
+  // Send mail
+  document.querySelector('#compose-form').addEventListener('submit', () => {
 
+    event.preventDefault();
+    send_email();
     load_mailbox('sent');
 
-    return false;
-  }
+  });
 
 
-  // By default, load the inbox
-  load_mailbox('inbox');
 });
+
+function read_email(email){
+
+  // Hide mailbox
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#emails-read').style.display = 'block';
+
+  // get mail form API
+  fetch(`/emails/${email}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+    // render mail
+    document.querySelector('#sender').innerHTML = `From: <b>${email.sender}</b>`;
+    document.querySelector('#recipients').innerHTML = `To: <b>${email.recipients}</b>`;
+    document.querySelector('#subject').innerHTML = `<h3>${email.subject}</h3>`;
+    document.querySelector('#timestamp').innerHTML = `${email.timestamp}`;
+    document.querySelector('#text').innerHTML = `${email.body}`;
+
+
+  });
+
+}
+
+function send_email(){
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value,
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+        console.log(result);
+      
+  });
+}
 
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#emails-read').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -47,10 +85,10 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#emails-read').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
 
 
   // Load mails
@@ -69,13 +107,12 @@ function load_mailbox(mailbox) {
         }
 
         document.querySelector('#emails-view').innerHTML +=
-          `<div class="mail">
-        <div class="sender">${user}</div>
-        <div class="subject">${single_mail.subject}</div>
-        <div class="timestamp">${single_mail.timestamp}</div>
-        </div>`;
+          `<div class="mail" id="mail" onclick="read_email(${single_mail.id})">
+          <div class="sender">${user}</div>
+          <div class="subject">${single_mail.subject}</div>
+          <div class="timestamp">${single_mail.timestamp}</div>
+          </div>`;
       });
-
-
     });
+
 }
